56e56f8a69248e3abeb17c57f6a98de48b5bec8b
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2019-10-15 10:22:26 +0200

    Retrieve block height from DB

--- README.md
+++ README.md
@@ -2,10 +2,22 @@
 
 > A JavaScript tool to scan smart contract events on Ethereum
 
-## Installation and Configuration
+## Configuration
 
-1. In `config/crontab` define the frequency the service should scan
-for new blocks.
+To create a config, do the following:
+
+```
+$ mv config/index_template.json config/index.json
+```
+
+Fill in all the relevant information. The template file contains a rough
+documentation of the fields.
+
+Additionally, in `config/crontab`, define the frequency with which the
+service should scan for new blocks.
+
+## Installation
+
 1. Build the container using: `sudo docker build --rm -t event-scanner .`
 1. And to run it in the foreground: `sudo docker run -t -i event-scanner`
 
--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log("Hello World");
+// @format
+const Web3 = require("web3");
+const levelup = require("levelup");
+const leveldown = require("leveldown");
+const allSettled = require("promise.allsettled");
+
+const config = require("./config");
+const web3 = new Web3(config.endpoint);
+
+async function run() {
+  const { db, contracts } = init();
+  const heights = await getHeights(db, contracts);
+}
+
+async function getHeights(db, contracts) {
+  const queries = contracts.map(c => `${c.options.address}_height`);
+  let p = queries.map(q => db.get(q));
+
+  // NOTE: levelup is throwing an error if a key is not available yet.
+  // We use the new ESNext compliant `allSettled` functionality to
+  // filter by keys that haven't been defined yet and ...
+  const results = await allSettled(p);
+  const heights = results.map(r => (r.status === "rejected" ? 0 : parseInt(r.value, 10)));
+
+  // ... we define all values that throw errors.
+  p = heights.reduce(
+    (acc, curr, i) =>
+      // NOTE: We're using `concat` instead of `push` here to return the
+      // update array to the reduce function.
+      curr === 0 ? acc.concat([contracts[i].options.address]) : acc,
+    []
+  ).map(c => db.put(`${c}_height`, "0"));
+  await Promise.all(p)
+
+  return heights;
+}
+
+function init() {
+  const contracts = [];
+  for (let i = 0; i < config.contracts.length; i++) {
+    contracts.push(
+      new web3.eth.Contract(
+        config.contracts[i].ABI,
+        config.contracts[i].address
+      )
+    );
+  }
+  return {
+    contracts,
+    db: levelup(leveldown(config.database.path))
+  };
+}
+
+run();
